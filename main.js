f751b4e30bd3e6beb96e6c15b3d22f6c79a8f206
Truman <liuyumeng0312@qq.com>
2017-06-27 17:55:02 +0800

    修改文件

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import store from './vuex/store'
-import App from './components/App.vue'
+import store from './src/vuex/store'
+import App from './src/components/App.vue'
 
 new Vue({
   store, // inject store to all children
